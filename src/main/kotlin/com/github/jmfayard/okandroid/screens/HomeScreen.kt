330e18150955c50b22e78b25f54f030963c5b25f
jmfayard <jean-michel.fayard@smartmobilefactory.com>
2017-05-17 15:46:12 +0200

    Do RecyclerView with SlimAdapter

--- src/main/kotlin/com/github/jmfayard/okandroid/screens/HomeScreen.kt
+++ src/main/kotlin/com/github/jmfayard/okandroid/screens/HomeScreen.kt
@@ -3,18 +3,23 @@ package com.github.jmfayard.okandroid.screens
 import android.content.Context
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
-import android.view.LayoutInflater
-import android.view.ViewGroup
-import com.github.jmfayard.okandroid.R
-import com.github.jmfayard.okandroid.databinding.ActivityStartBinding
-import com.github.jmfayard.okandroid.databinding.ActivityStartBinding.inflate
-import com.github.jmfayard.okandroid.databinding.ItemHomeBinding
-import com.github.jmfayard.okandroid.inflater
-import com.github.jmfayard.okandroid.toast
+import com.github.jmfayard.okandroid.*
 import com.github.jmfayard.okandroid.utils.See
+import com.wealthfront.magellan.BaseScreenView
 import com.wealthfront.magellan.Screen
+import kotterknife.bindView
+import net.idik.lib.slimadapter.SlimAdapter
+
+interface ListItem
 
 @See(layout = R.layout.item_home)
+data class HomeItem(val title: String, val description: String, val screen: Screen<*>) : ListItem
+
+@See(layout = R.layout.item_setion_header)
+data class HeaderItem(val title: String) : ListItem
+
+
+@See(layout = R.layout.activity_start)
 class HomeScreen : com.wealthfront.magellan.Screen<HomeView>() {
 
     override fun createView(context: android.content.Context): HomeView {
@@ -28,56 +33,44 @@ class HomeScreen : com.wealthfront.magellan.Screen<HomeView>() {
     fun onItemClicked(item: HomeItem) {
         navigator.goTo(item.screen)
     }
-}
 
-val ITEMS = listOf(
-        HomeItem("TagsScreen", "Try #android #features", TagsScreen()),
-        HomeItem("RegisterScreen", "Registration by SMS flow", RegisterScreen()),
-        HomeItem("RxScreen", "RxJava / RxBinding", RxScreen())
-)
+    val ITEMS: List<ListItem> = listOf(
+            HeaderItem("Current Stuff"),
+            HomeItem("TagsScreen", "Try #android #features", TagsScreen()),
+            HomeItem("RegisterScreen", "Registration by SMS flow", RegisterScreen()),
+            HeaderItem("Old Stuff"),
+            HomeItem("RxScreen", "RxJava / RxBinding", RxScreen())
+    )
 
-data class HomeItem(val title: String, val description: String, val screen: Screen<*>)
-
-class HomeAdapter(val context: Context, val items: List<HomeItem>, val onClick: (HomeItem)->Unit) : RecyclerView.Adapter<HomeHolder>() {
-
-    val inflater = LayoutInflater.from(context)
-
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): HomeHolder {
-        val binding : ItemHomeBinding = ItemHomeBinding.inflate(inflater, parent, false)
-        return HomeHolder(binding)
-    }
-
-    override fun onBindViewHolder(holder: HomeHolder, position: Int) {
-        holder.bind(items[position], onClick)
-    }
-
-    override fun getItemCount(): Int = items.size
-}
-
-class HomeHolder(val binding: ItemHomeBinding) : RecyclerView.ViewHolder(binding.root) {
-    fun bind(item: HomeItem, onClick: (HomeItem) -> Unit) {
-        binding.homeItemTitle.text = item.title
-        binding.homeItemDescription.text = item.description
-        binding.cardView.setOnClickListener { onClick(item) }
-        binding.executePendingBindings()
+    override fun onShow(context: Context?) {
+        view.slimAdapter.updateData(ITEMS)
     }
 
 }
 
 
+class HomeView(context: android.content.Context) : BaseScreenView<HomeScreen>(context) {
 
-class HomeView (context: android.content.Context) : com.wealthfront.magellan.BaseScreenView<HomeScreen>(context) {
+    val list: RecyclerView by bindView(R.id.list)
 
-    val binding : ActivityStartBinding = inflate(inflater, this, true)
+    val slimAdapter by lazy {
+        SlimAdapter.create()
+                .register<HomeItem>(R.layout.item_home) { data: HomeItem, injector ->
+                    injector.text(R.id.home_item_title, data.title)
+                            .text(R.id.home_item_description, data.description)
+                            .clicked(R.id.card_view, { _ -> screen.onItemClicked(data) })
+                }
+                .register<HeaderItem>(R.layout.item_setion_header) { item: HeaderItem, injector ->
+                    injector.text(R.id.section_title, item.title)
+                }
+                .attachTo(list)
+    }
 
     init {
-
-        with (binding.recycler) {
+        inflater.inflate(R.layout.activity_start, this, attach)
+        with(list) {
             layoutManager = LinearLayoutManager(context)
-            adapter = HomeAdapter(context, ITEMS) { item  : HomeItem ->
-                toast("Clicked on ${item}")
-                screen.onItemClicked(item)
-            }
+            adapter = slimAdapter
         }
     }
 
