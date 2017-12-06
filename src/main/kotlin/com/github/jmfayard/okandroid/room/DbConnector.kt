ce8ac94b5c053e918b80f6970e47c677ca3482a1
jmfayard <jean-michel@mautinoa.com>
2017-12-06 11:15:12 +0100

    Update dependencies

--- src/main/kotlin/com/github/jmfayard/okandroid/room/DbConnector.kt
+++ src/main/kotlin/com/github/jmfayard/okandroid/room/DbConnector.kt
@@ -4,22 +4,20 @@ import io.reactivex.Flowable
 import io.reactivex.Scheduler
 import io.reactivex.Single
 import io.reactivex.android.schedulers.AndroidSchedulers
-import io.reactivex.rxkotlin.toSingle
 import io.reactivex.schedulers.Schedulers
+
 
 class DbConnector(
         val subscribeOn: Scheduler = Schedulers.io(),
         val observeOn: Scheduler = AndroidSchedulers.mainThread()) {
 
     fun delete(person: Person): Single<Unit> =
-            { DB.albums().delete(person) }
-                    .toSingle()
+            Single.fromCallable { DB.albums().delete(person) }
                     .subscribeOn(subscribeOn)
                     .observeOn(observeOn)
 
     fun insert(person: Person): Single<Unit> =
-            { DB.albums().insert(person) }
-                    .toSingle()
+            Single.fromCallable { DB.albums().insert(person) }
                     .subscribeOn(subscribeOn)
                     .observeOn(observeOn)
 
@@ -29,8 +27,8 @@ class DbConnector(
                 .observeOn(observeOn)
     }
 
-    fun deleteAll(): Single<Int> = { DB.albums().deleteAll() }
-            .toSingle()
-            .subscribeOn(subscribeOn)
-            .observeOn(observeOn)
+    fun deleteAll(): Single<Int> =
+            Single.fromCallable { DB.albums().deleteAll() }
+                    .subscribeOn(subscribeOn)
+                    .observeOn(observeOn)
 }
