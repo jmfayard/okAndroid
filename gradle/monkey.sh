86641daea958d6ac5dafd69e49f99b9e99256a20
jmfayard <jean-michel.fayard@smartmobilefactory.com>
2018-02-06 17:58:14 +0100

    $ gw monkey

--- gradle/monkey.sh
+++ gradle/monkey.sh
@@ -1,27 +1,28 @@
 #!/usr/bin/env bash
-export ITERATIONS=${1:-1}
-export PACKAGE="com.mautinoa.app.fabric"
+export PACKAGE=$1
+export ITERATIONS=5
+export EVENTS=100
 export FABRIC="https://goo.gl/Hd9vDn"
 echo "Starting $PACKAGE"
 
-test "$ITERATIONS" == "install" && {
-    echo "Building and installing $PACKAGE"
-    ./gradlew app:installFabricDebug
-    exit 0
-}
+#test "$ITERATIONS" == "install" && {
+#    echo "Building and installing $PACKAGE"
+#    ./gradlew app:installFabricDebug
+#    exit 0
+#}
 
-export START_OK=$( adb shell am start -n ${PACKAGE}/com.mautinoa.client.MainActivity 2>&1 | grep -i error )
-test -z "$START_OK" || {
-    echo "Please install the latest fabric release of ${PACKAGE} from ${FABRIC}"
-    adb shell am start -a android.intent.action.VIEW -d "${FABRIC}"
-    exit 1
-}
+#export START_OK=$( adb shell am start -n ${PACKAGE}/com.mautinoa.client.MainActivity 2>&1 | grep -i error )
+#test -z "$START_OK" || {
+#    echo "Please install the latest fabric release of ${PACKAGE} from ${FABRIC}"
+#    adb shell am start -a android.intent.action.VIEW -d "${FABRIC}"
+#    exit 1
+#}
 
 echo "Will run ${ITERATIONS} iterations on ${PACKAGE} of the evil chaos monkey: "
-echo "$ adb shell monkey -p ${PACKAGE} -v 5000"
+echo "$ adb shell monkey -p ${PACKAGE} -v ${EVENTS}"
 
 sleep 2
 for i in $( seq 1 ${ITERATIONS} ) ; do
-  adb shell monkey -p "${PACKAGE}" -v 5000
+  adb shell monkey -p "${PACKAGE}" -v ${EVENTS}
 done
 
