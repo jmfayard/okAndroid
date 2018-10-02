cffa1fe794f295280b20412595f3b4708f7296a6
jmfayard <jean-michel.fayard@smartmobilefactory.com>
2018-10-02 21:56:54 +0200

    $ ./gradlew syncLibs    # from plugin gradle-kotlin-dsl-libs

---
+++ buildSrc/src/main/java/Libs.kt
@@ -0,0 +1,415 @@
+import kotlin.String
+
+object Libs {
+    /**
+     * [aapt2 website](https://developer.android.com/studio) */
+    const val aapt2: String = "com.android.tools.build:aapt2:" + Versions.aapt2
+
+    /**
+     * [android-job website](https://github.com/evernote/android-job) */
+    const val android_job: String = "com.evernote:android-job:" + Versions.android_job
+
+    /**
+     * [appcompat website](http://developer.android.com/tools/extras/support-library.html) */
+    const val appcompat: String = "androidx.appcompat:appcompat:" + Versions.appcompat
+
+    /**
+     * [browser website](http://developer.android.com/tools/extras/support-library.html) */
+    const val browser: String = "androidx.browser:browser:" + Versions.browser
+
+    /**
+     * [calligraphy website](https://github.com/chrisjenx/Calligraphy) */
+    const val calligraphy: String = "uk.co.chrisjenx:calligraphy:" + Versions.calligraphy
+
+    /**
+     * [cardview website](http://developer.android.com/tools/extras/support-library.html) */
+    const val cardview: String = "androidx.cardview:cardview:" + Versions.cardview
+
+    const val com_gradle_build_scan_gradle_plugin: String =
+            "com.gradle.build-scan:com.gradle.build-scan.gradle.plugin:" + Versions.com_gradle_build_scan_gradle_plugin
+
+    /**
+     * [constraintlayout website](http://tools.android.com) */
+    const val constraintlayout: String =
+            "androidx.constraintlayout:constraintlayout:" + Versions.constraintlayout
+
+    /**
+     * [converter-moshi website](https://github.com/square/retrofit/) */
+    const val converter_moshi: String =
+            "com.squareup.retrofit2:converter-moshi:" + Versions.converter_moshi
+
+    const val core: String = "com.afollestad.material-dialogs:core:" + Versions.core
+
+    /**
+     * [core-ktx website](http://developer.android.com/tools/extras/support-library.html) */
+    const val core_ktx: String = "androidx.core:core-ktx:" + Versions.core_ktx
+
+    /**
+     * [espresso-contrib website](https://developer.android.com/testing) */
+    const val espresso_contrib: String =
+            "androidx.test.espresso:espresso-contrib:" + Versions.espresso_contrib
+
+    /**
+     * [espresso-core website](https://developer.android.com/testing) */
+    const val espresso_core: String =
+            "androidx.test.espresso:espresso-core:" + Versions.espresso_core
+
+    /**
+     * [gradle website](https://developer.android.com/studio) */
+    const val gradle: String = "com.android.tools.build:gradle:" + Versions.gradle
+
+    const val javax_inject: String = "javax.inject:javax.inject:" + Versions.javax_inject
+
+    const val jmfayard_github_io_gradle_kotlin_dsl_libs_gradle_plugin: String =
+            "jmfayard.github.io.gradle-kotlin-dsl-libs:jmfayard.github.io.gradle-kotlin-dsl-libs.gradle.plugin:" + Versions.jmfayard_github_io_gradle_kotlin_dsl_libs_gradle_plugin
+
+    /**
+     * [jsr305 website](http://findbugs.sourceforge.net/) */
+    const val jsr305: String = "com.google.code.findbugs:jsr305:" + Versions.jsr305
+
+    /**
+     * [junit website](http://junit.org) */
+    const val junit: String = "junit:junit:" + Versions.junit
+
+    /**
+     * [kotlin-android-extensions website](https://kotlinlang.org/) */
+    const val kotlin_android_extensions: String =
+            "org.jetbrains.kotlin:kotlin-android-extensions:" + Versions.kotlin_android_extensions
+
+    /**
+     * [kotlin-annotation-processing-gradle website](https://kotlinlang.org/) */
+    const val kotlin_annotation_processing_gradle: String =
+            "org.jetbrains.kotlin:kotlin-annotation-processing-gradle:" + Versions.kotlin_annotation_processing_gradle
+
+    /**
+     * [kotlin-coroutines-retrofit website](https://github.com/gildor/kotlin-coroutines-retrofit) */
+    const val kotlin_coroutines_retrofit: String =
+            "ru.gildor.coroutines:kotlin-coroutines-retrofit:" + Versions.kotlin_coroutines_retrofit
+
+    /**
+     * [kotlin-gradle-plugin website](https://kotlinlang.org/) */
+    const val kotlin_gradle_plugin: String =
+            "org.jetbrains.kotlin:kotlin-gradle-plugin:" + Versions.kotlin_gradle_plugin
+
+    /**
+     * [kotlin-reflect website](https://kotlinlang.org/) */
+    const val kotlin_reflect: String =
+            "org.jetbrains.kotlin:kotlin-reflect:" + Versions.kotlin_reflect
+
+    /**
+     * [kotlin-scripting-compiler-embeddable website](https://kotlinlang.org/) */
+    const val kotlin_scripting_compiler_embeddable: String =
+            "org.jetbrains.kotlin:kotlin-scripting-compiler-embeddable:" + Versions.kotlin_scripting_compiler_embeddable
+
+    /**
+     * [kotlin-stdlib website](https://kotlinlang.org/) */
+    const val kotlin_stdlib: String = "org.jetbrains.kotlin:kotlin-stdlib:" + Versions.kotlin_stdlib
+
+    /**
+     * [kotlin-stdlib-jdk8 website](https://kotlinlang.org/) */
+    const val kotlin_stdlib_jdk8: String =
+            "org.jetbrains.kotlin:kotlin-stdlib-jdk8:" + Versions.kotlin_stdlib_jdk8
+
+    const val kotlinandroidviewbindings: String =
+            "com.github.MarcinMoskala:KotlinAndroidViewBindings:" + Versions.kotlinandroidviewbindings
+
+    /**
+     * [kotlintest website](http://www.github.com/kotlintest/kotlintest) */
+    const val kotlintest: String = "io.kotlintest:kotlintest:" + Versions.kotlintest
+
+    /**
+     * [kotlinx-coroutines-core website](https://github.com/Kotlin/kotlinx.coroutines) */
+    const val kotlinx_coroutines_core: String =
+            "org.jetbrains.kotlinx:kotlinx-coroutines-core:" + Versions.kotlinx_coroutines_core
+
+    const val ktor_features: String = "io.ktor:ktor-features:" + Versions.ktor_features
+
+    const val ktor_server_core: String = "io.ktor:ktor-server-core:" + Versions.ktor_server_core
+
+    const val ktor_server_netty: String = "io.ktor:ktor-server-netty:" + Versions.ktor_server_netty
+
+    /**
+     * [legacy-support-v4 website](http://developer.android.com/tools/extras/support-library.html) */
+    const val legacy_support_v4: String =
+            "androidx.legacy:legacy-support-v4:" + Versions.legacy_support_v4
+
+    /**
+     * [lint-gradle website](https://developer.android.com/studio) */
+    const val lint_gradle: String = "com.android.tools.lint:lint-gradle:" + Versions.lint_gradle
+
+    /**
+     * [logging-interceptor website](https://github.com/square/okhttp) */
+    const val logging_interceptor: String =
+            "com.squareup.okhttp3:logging-interceptor:" + Versions.logging_interceptor
+
+    /**
+     * [magellan website](https://github.com/wealthfront/magellan/) */
+    const val magellan: String = "com.wealthfront:magellan:" + Versions.magellan
+
+    /**
+     * [magellan-rx website](https://github.com/wealthfront/magellan/) */
+    const val magellan_rx: String = "com.wealthfront:magellan-rx:" + Versions.magellan_rx
+
+    /**
+     * [magellan-support website](https://github.com/wealthfront/magellan/) */
+    const val magellan_support: String =
+            "com.wealthfront:magellan-support:" + Versions.magellan_support
+
+    /**
+     * [material website](http://developer.android.com/tools/extras/support-library.html) */
+    const val material: String = "com.google.android.material:material:" + Versions.material
+
+    /**
+     * [mediarouter website](http://developer.android.com/tools/extras/support-library.html) */
+    const val mediarouter: String = "androidx.mediarouter:mediarouter:" + Versions.mediarouter
+
+    /**
+     * [mockito-kotlin website](https://github.com/nhaarman/mockito-kotlin) */
+    const val mockito_kotlin: String = "com.nhaarman:mockito-kotlin:" + Versions.mockito_kotlin
+
+    /**
+     * [mockwebserver website](https://github.com/square/okhttp) */
+    const val mockwebserver: String = "com.squareup.okhttp3:mockwebserver:" + Versions.mockwebserver
+
+    /**
+     * [moneta-bp website](http://java.net/projects/javamoney) */
+    const val moneta_bp: String = "org.javamoney:moneta-bp:" + Versions.moneta_bp
+
+    /**
+     * [moshi website](https://github.com/square/moshi) */
+    const val moshi: String = "com.squareup.moshi:moshi:" + Versions.moshi
+
+    const val multidex: String = "androidx.multidex:multidex:" + Versions.multidex
+
+    /**
+     * [okhttp website](https://github.com/square/okhttp) */
+    const val okhttp: String = "com.squareup.okhttp3:okhttp:" + Versions.okhttp
+
+    /**
+     * [okio website](https://github.com/square/okio/) */
+    const val okio: String = "com.squareup.okio:okio:" + Versions.okio
+
+    /**
+     * [percentlayout website](http://developer.android.com/tools/extras/support-library.html) */
+    const val percentlayout: String =
+            "androidx.percentlayout:percentlayout:" + Versions.percentlayout
+
+    /**
+     * [preference website](http://developer.android.com/tools/extras/support-library.html) */
+    const val preference: String = "androidx.preference:preference:" + Versions.preference
+
+    /**
+     * [retrofit website](https://github.com/square/retrofit/) */
+    const val retrofit: String = "com.squareup.retrofit2:retrofit:" + Versions.retrofit
+
+    const val retrofit2_rxjava2_adapter: String =
+            "com.jakewharton.retrofit:retrofit2-rxjava2-adapter:" + Versions.retrofit2_rxjava2_adapter
+
+    /**
+     * [retrofit-mock website](https://github.com/square/retrofit/) */
+    const val retrofit_mock: String =
+            "com.squareup.retrofit2:retrofit-mock:" + Versions.retrofit_mock
+
+    /**
+     * [room-compiler website](https://developer.android.com/topic/libraries/architecture/index.html) */
+    const val room_compiler: String = "androidx.room:room-compiler:" + Versions.room_compiler
+
+    /**
+     * [room-runtime website](https://developer.android.com/topic/libraries/architecture/index.html) */
+    const val room_runtime: String = "androidx.room:room-runtime:" + Versions.room_runtime
+
+    /**
+     * [room-rxjava2 website](https://developer.android.com/topic/libraries/architecture/index.html) */
+    const val room_rxjava2: String = "androidx.room:room-rxjava2:" + Versions.room_rxjava2
+
+    /**
+     * [room-testing website](https://developer.android.com/topic/libraries/architecture/index.html) */
+    const val room_testing: String = "androidx.room:room-testing:" + Versions.room_testing
+
+    /**
+     * [runner website](https://developer.android.com/testing) */
+    const val runner: String = "androidx.test:runner:" + Versions.runner
+
+    /**
+     * [rxandroid website](https://github.com/ReactiveX/RxAndroid) */
+    const val rxandroid: String = "io.reactivex.rxjava2:rxandroid:" + Versions.rxandroid
+
+    /**
+     * [rxbinding-kotlin website](https://github.com/JakeWharton/RxBinding/) */
+    const val rxbinding_kotlin: String =
+            "com.jakewharton.rxbinding2:rxbinding-kotlin:" + Versions.rxbinding_kotlin
+
+    /**
+     * [rxbinding-support-v4-kotlin website](https://github.com/JakeWharton/RxBinding/) */
+    const val rxbinding_support_v4_kotlin: String =
+            "com.jakewharton.rxbinding2:rxbinding-support-v4-kotlin:" + Versions.rxbinding_support_v4_kotlin
+
+    /**
+     * [rxjava website](https://github.com/ReactiveX/RxJava) */
+    const val rxjava: String = "io.reactivex.rxjava2:rxjava:" + Versions.rxjava
+
+    /**
+     * [rxkotlin website](https://github.com/ReactiveX/RxKotlin) */
+    const val rxkotlin: String = "io.reactivex.rxjava2:rxkotlin:" + Versions.rxkotlin
+
+    /**
+     * [rxpermissions website](https://github.com/tbruyelle/RxPermissions) */
+    const val rxpermissions: String =
+            "com.tbruyelle.rxpermissions2:rxpermissions:" + Versions.rxpermissions
+
+    /**
+     * [slimadapter website](https://github.com/MEiDIK/SlimAdapter) */
+    const val slimadapter: String = "net.idik:slimadapter:" + Versions.slimadapter
+
+    /**
+     * [timber website](https://github.com/JakeWharton/timber) */
+    const val timber: String = "com.jakewharton.timber:timber:" + Versions.timber
+
+    object Versions {
+        const val aapt2: String = "3.2.0-4818971" // available: milestone=3.3.0-alpha13-5013011
+
+        const val android_job: String = "1.2.1" // available: milestone=1.3.0-alpha07
+
+        const val appcompat: String = "1.0.0-rc01" // available: milestone=1.0.0
+
+        const val browser: String = "1.0.0-rc01" // available: milestone=1.0.0
+
+        const val calligraphy: String = "2.1.0" // up-to-date
+
+        const val cardview: String = "1.0.0-rc01" // available: milestone=1.0.0
+
+        const val com_gradle_build_scan_gradle_plugin: String = "1.16" // up-to-date
+
+        const val constraintlayout: String = "1.1.3" // available: milestone=2.0.0-alpha2
+
+        const val converter_moshi: String = "2.4.0" // up-to-date
+
+        const val core: String = "0.9.6.0" // available: milestone=2.0.0-beta3
+
+        const val core_ktx: String = "1.0.0-rc02" // available: milestone=1.0.0
+
+        const val espresso_contrib: String = "3.1.0-alpha1" // available: milestone=3.1.0-alpha4
+
+        const val espresso_core: String = "3.1.0-alpha1" // available: milestone=3.1.0-alpha4
+
+        const val gradle: String = "3.2.0" // available: milestone=3.3.0-alpha13
+
+        const val javax_inject: String = "1" 
+                /* error: Could not find any matches for javax.inject:javax.inject:+ as no versions of javax.inject:javax.inject are available.
+                Searched in the following locations:
+                  - file:/Users/jmfayard/Dev/android-sdk-macosx/extras/m2repository/javax/inject/javax.inject/maven-metadata.xml
+                  - file:/Users/jmfayard/Dev/android-sdk-macosx/extras/m2repository/javax/inject/javax.inject/ 
+                .... */
+
+        const val jmfayard_github_io_gradle_kotlin_dsl_libs_gradle_plugin: String =
+                "0.2.0" // exceed the version found: 0.1
+
+        const val jsr305: String = "3.0.2" // up-to-date
+
+        const val junit: String = "4.12" // up-to-date
+
+        const val kotlin_android_extensions: String = "1.2.60" // up-to-date
+
+        const val kotlin_annotation_processing_gradle: String = "1.2.60" // up-to-date
+
+        const val kotlin_coroutines_retrofit: String = "0.9.0" // available: milestone=0.12.0
+
+        const val kotlin_gradle_plugin: String = "1.2.60" // available: milestone=1.2.71
+
+        const val kotlin_reflect: String = "1.2.60" // up-to-date
+
+        const val kotlin_scripting_compiler_embeddable: String = "1.2.60" // up-to-date
+
+        const val kotlin_stdlib: String = "1.2.60" // up-to-date
+
+        const val kotlin_stdlib_jdk8: String = "1.2.60" // up-to-date
+
+        const val kotlinandroidviewbindings: String = "0.12" // up-to-date
+
+        const val kotlintest: String = "2.0.7" // up-to-date
+
+        const val kotlinx_coroutines_core: String = "0.21.1" // available: milestone=0.30.1
+
+        const val ktor_features: String = "0.9.1" // available: milestone=0.9.6-alpha-1-rc13
+
+        const val ktor_server_core: String = "0.9.1" // available: milestone=0.9.6-alpha-1-rc13
+
+        const val ktor_server_netty: String = "0.9.1" // available: milestone=0.9.6-alpha-1-rc13
+
+        const val legacy_support_v4: String = "1.0.0-rc01" // available: milestone=1.0.0
+
+        const val lint_gradle: String = "26.2.0" // available: milestone=26.3.0-alpha13
+
+        const val logging_interceptor: String = "3.11.0" // up-to-date
+
+        const val magellan: String = "1.1.0" // up-to-date
+
+        const val magellan_rx: String = "1.1.0" // up-to-date
+
+        const val magellan_support: String = "1.1.0" // up-to-date
+
+        const val material: String = "1.0.0-rc01" // available: milestone=1.0.0
+
+        const val mediarouter: String = "1.0.0-alpha1" // available: milestone=1.0.0
+
+        const val mockito_kotlin: String = "1.5.0" // up-to-date
+
+        const val mockwebserver: String = "3.11.0" // up-to-date
+
+        const val moneta_bp: String = "1.3" // up-to-date
+
+        const val moshi: String = "1.6.0" // available: milestone=1.7.0
+
+        const val multidex: String = "2.0.0" // up-to-date
+
+        const val okhttp: String = "3.11.0" // up-to-date
+
+        const val okio: String = "2.0.0" // available: milestone=2.1.0
+
+        const val percentlayout: String = "1.0.0-rc01" // available: milestone=1.0.0
+
+        const val preference: String = "1.0.0-rc01" // available: milestone=1.0.0
+
+        const val retrofit: String = "2.4.0" // up-to-date
+
+        const val retrofit2_rxjava2_adapter: String = "1.0.0" // up-to-date
+
+        const val retrofit_mock: String = "2.4.0" // up-to-date
+
+        const val room_compiler: String = "2.0.0-beta01" // available: milestone=2.0.0
+
+        const val room_runtime: String = "2.0.0-beta01" // available: milestone=2.0.0
+
+        const val room_rxjava2: String = "2.0.0-beta01" // available: milestone=2.0.0
+
+        const val room_testing: String = "2.0.0-beta01" // available: milestone=2.0.0
+
+        const val runner: String = "1.1.0-alpha1" // available: milestone=1.1.0-alpha4
+
+        const val rxandroid: String = "2.0.1" // up-to-date
+
+        const val rxbinding_kotlin: String = "2.1.1" // available: milestone=2.2.0
+
+        const val rxbinding_support_v4_kotlin: String = "2.1.1" // available: milestone=2.2.0
+
+        const val rxjava: String = "2.2.2" // up-to-date
+
+        const val rxkotlin: String = "2.3.0" // up-to-date
+
+        const val rxpermissions: String = "0.9.5" // up-to-date
+
+        const val slimadapter: String = "2.1.2" // up-to-date
+
+        const val timber: String = "4.7.1" // up-to-date
+    }
+
+    object Gradle {
+        const val runningVersion: String = "4.10"
+
+        const val currentVersion: String = "4.10.2"
+
+        const val nightlyVersion: String = "5.0-20181002174654+0000"
+
+        const val releaseCandidate: String = ""
+    }
+}
